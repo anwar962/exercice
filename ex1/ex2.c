75f14f6a0a53671117ac6b7a0cd542bcacffde23
anwar962 <elmousaouyanwar31@gmail.com>
2025-09-03 11:47:07 +0200

    Updated code

---
+++ ex1/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i,n;
+    int factoriel=1;
+    printf("entrez un nombre entier");
+    scanf("%d"&n);
+    if(n<0){
+        printf("entrez un nombre entiez positif");}
+        else{
+            for(i=1;i<=n;i++)
+            factoriel=F*i;
+        printf(le factoriel de %d est:%d,);}
+
+        return 0;
+}
